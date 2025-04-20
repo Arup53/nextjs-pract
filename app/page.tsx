5f1b0dba8664b373a9d77b7fc6d01bf3631a709e
Arup53 <arupkumarhowlader12@gmail.com>
2025-04-20 12:07:46 +0600

    add separate route for home and dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Test from "@/components/testcomponent/page";
 
 export default function Home() {
-  return (
-    <div>
-      <Test user={{ name: "koi", age: 25 }} />
-    </div>
-  );
+  return <div>{/* <Test user={{ name: "koi", age: 25 }} /> */}</div>;
 }
