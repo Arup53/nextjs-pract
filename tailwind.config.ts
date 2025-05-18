92359c40d91cf0ee125d33c43c3d15299c0c0dd3
Arup53 <arupkumarhowlader12@gmail.com>
2025-05-18 11:33:20 +0600

    modify tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "tailwindcss";
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
+
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
 const config: Config = {
   darkMode: ["class"],
